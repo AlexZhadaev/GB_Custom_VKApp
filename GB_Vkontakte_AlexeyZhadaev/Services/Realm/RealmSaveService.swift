7600a02a0bf20a1d3eb5650a05fc672bf4f33a35
AlexZhadaev <pvt.vatnik@gmail.com>
2020-10-18 22:51:07 +0300

    Realm install RealmSaveService added

---
+++ GB_Vkontakte_AlexeyZhadaev/Services/Realm/RealmSaveService.swift
@@ -0,0 +1,64 @@
+//
+//  RealmSaveService.swift
+//  GB_Vkontakte_AlexeyZhadaev
+//
+//  Created by Жадаев Алексей on 18.10.2020.
+//  Copyright © 2020 Жадаев Алексей. All rights reserved.
+//
+
+import RealmSwift
+
+class UserObject: Object {
+   @objc dynamic var firstName = ""
+    @objc dynamic var lastName = ""
+    @objc dynamic var avatar = ""
+    @objc dynamic var id: Int = 0
+}
+
+class RealmSaveService: SaveServiceInterface {
+    
+    let realm: Realm
+    
+    init() {
+        let config = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
+        realm = try! Realm(configuration: config)
+    }
+    
+    func saveUser(firstName: String, lastName: String, avatar: String, id: Int) {
+        let user = UserObject()
+        user.firstName = firstName
+        user.lastName = lastName
+        user.avatar = avatar
+        user.id = id
+        
+        try! realm.write {
+            realm.add(user)
+        }
+    }
+    
+    func readUserList(completion: @escaping ([UserEntity]) -> Void) {
+        let realm = try! Realm()
+        let list = realm.objects(UserObject.self)
+        completion (list.map {UserEntity (firstName: $0.firstName, lastName: $0.lastName, avatar: $0.avatar, id:  $0.id) } )
+    }
+    
+    func deleteEntityList(entity: String) {
+
+    }
+    
+    func saveGroup(name: String, avatar: String) {
+
+    }
+    
+    func readGroupList(completion: @escaping ([GroupEntity]) -> Void) {
+
+    }
+    
+    func savePhoto(userLikes: Int, likesCount: Int, url: String) {
+
+    }
+    
+    func readPhotoList(completion: @escaping ([PhotoEntity]) -> Void) {
+
+    }
+}
--- Podfile
+++ Podfile
@@ -8,5 +8,16 @@ target 'GB_Vkontakte_AlexeyZhadaev' do
   pod 'Alamofire', '~> 5.2'
 
   # Pods for GB_Vkontakte_AlexeyZhadaev
-
+	pod 'RealmSwift'
 end
+
+post_install do |installer|
+     installer.pods_project.targets.each do |target|
+         target.build_configurations.each do |config|
+             config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = 'arm64'
+             config.build_settings['EXCLUDED_ARCHS[sdk=watchsimulator*]'] = 'arm64'
+             config.build_settings['EXCLUDED_ARCHS[sdk=appletvsimulator*]'] = 'arm64'
+    
+         end
+     end
+ end
